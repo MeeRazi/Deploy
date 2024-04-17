354034b9bb80d1fb5609386f8f88beff55f15147
L E M O N <ryme.care@hotmail.com>
2024-04-18 00:19:31 +0530

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# Importing environment variables from Render.com
+SEC_KEY=$SECREAT_KEY
+
+# Now you can use VAR1 and VAR2 in your script
+echo "SEC_KEY: $SEC_KEY"
+
+git clone -b Pimon "https://$SEC_KEY@github.com/Lemx5/Rosie"
+
+cd /Rosie
+pip3 install -U -r requirements.txt
+echo "Starting Bot...."
+python3 bot.py
