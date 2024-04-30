7232e5a471edc8580a7ed1d518d6b47b27af49d9
L E M O N <ryme.care@hotmail.com>
2024-04-30 10:30:55 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -6,20 +6,7 @@ SEC_KEY=$SECREAT_KEY
 echo "SEC_KEY: $SEC_KEY"
 
 git clone -b Pimon "https://$SEC_KEY@github.com/Lemx5/Rosie"
-
-# Print the current directory
-current_dir=$(pwd)
-echo "Current directory: $current_dir"
-echo "Files in current directory:"
-ls -l
-
 cd /app/Rosie
-
-current_dir=$(pwd)
-echo "Current directory: $current_dir"
-echo "Files in current directory:"
-ls -l
-
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
