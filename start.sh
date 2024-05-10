1191f1adf37b89cc51f2b46cc600e2f7459ce3d7
L E M O N <ryme.care@hotmail.com>
2024-05-10 10:18:17 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 SEC_KEY=$SECREAT_KEY
 echo "Cloning Custom Repo"
-git clone SEC_KEY /EvaMaria
+git clone -b Pimon "https://$SEC_KEY@github.com/Lemx5/Rosie" /EvaMaria
 cd /EvaMaria
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
