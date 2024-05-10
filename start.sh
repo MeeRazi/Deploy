1002280651b5146e55da213960383ba1c3a8de61
L E M O N <ryme.care@hotmail.com>
2024-05-10 10:15:58 +0530

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,7 @@
+$UPSTREAM_REPO
+echo "Cloning Custom Repo from $UPSTREAM_REPO "
+git clone $UPSTREAM_REPO /EvaMaria
+cd /EvaMaria
+pip3 install -U -r requirements.txt
+echo "Starting Bot...."
+python3 bot.py
