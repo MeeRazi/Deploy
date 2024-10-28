db72028299e5d2604e1cd45c4807cc254ac889a7
R A Z i <119275831+MeeRazi@users.noreply.github.com>
2024-10-29 02:26:33 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 SEC_KEY=$SECREAT_KEY
 echo "Cloning Custom Repo"
-git clone -b main "https://$SEC_KEY@github.com/MeeRazi/Autofilter" /BotSync
+git clone -b main "https://$SEC_KEY@github.com/MeeRazi/AutoFilter.git" /BotSync
 cd /BotSync
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
