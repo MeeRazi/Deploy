c94e8d3ae18c42868a33631dc44c2dbef93df03e
R A Z i <119275831+MeeRazi@users.noreply.github.com>
2024-12-01 18:13:56 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,7 @@
 GIT_TOKEN=$GIT_TOKEN
+REPO=$REPO
 echo "Cloning Custom Repo"
-git clone -b main "https://$GIT_TOKEN@github.com/MeeRazi/AutoFilter.git" /BotSync
+git clone -b main "https://$GIT_TOKEN@github.com/MeeRazi/$REPO" /BotSync
 cd /BotSync
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
